0d36586204043f9e8b8e4eea763b14e97a763747
Claes Gyllensvärd <letharion@gmail.com>
2013-10-03 22:02:51 +0100

    Move the example files into the doc/ dir.

---
+++ doc/example.php
@@ -0,0 +1,28 @@
+<?php
+
+include __DIR__ . '/../validateaction.php';
+
+if (isset($_GET['receipt'])) {
+    $receipt  = $_GET['receipt'];
+}
+else {
+    print 'No receipt to validate. Exiting.<br />';
+    return;
+}
+
+$endpoint = isset($_GET['sandbox']) ? itunesReceiptValidator::SANDBOX_URL : itunesReceiptValidator::PRODUCTION_URL;
+
+try {
+    $rv = new itunesReceiptValidator($endpoint, 'fake_receipt');
+
+    print 'Environment: ' .
+      ($rv->getEndpoint() === itunesReceiptValidator::SANDBOX_URL) ? 'Sandbox' : 'Production' .
+      '<br />';
+
+    $info = $rv->validateReceipt();
+    echo 'Success';
+    var_dump($info);
+}
+catch (Exception $ex) {
+    echo $ex->getMessage() . '<br />';
+}
